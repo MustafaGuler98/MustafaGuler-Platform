8f9641bfce31c5e5e911290c3c77ab5ab5215ce0
Semih Kenan Turgut <semihkenan12@gmail.com>
2025-12-11 21:48:19 +0300

    Add static metadata and resolve metadata bugs

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,15 @@
 import { articleService } from "@/services/articleServices";
 import PortalClient from "./portal-client";
+import { constructMetadata } from "@/lib/seo";
+import { get } from "http";
+import { getImageUrl } from "@/lib/utils";
 
+export const metadata = constructMetadata({
+  title: "Portal",
+  description: "Mustafa Guler's digital garden. Exploring software architecture, .NET, Next.js and cyberpunk aesthetics.",
+  path: "/",
+  image: getImageUrl("/assets/images/logo1.png"),
+});
 export default async function Home() {
   const allArticles = await articleService.getAllArticles();
 
