732820775a9fcbd931eb1fd637b76c3165f04af9
Mustafa Güler <mail.mustafaguler@gmail.com>
2026-01-11 16:34:19 +0300

    Add LoginDtoValidator

---
+++ backend/MustafaGuler.Service/Validators/LoginDtoValidator.cs
@@ -0,0 +1,19 @@
+using FluentValidation;
+using MustafaGuler.Core.DTOs;
+
+namespace MustafaGuler.Service.Validators
+{
+    public class LoginDtoValidator : AbstractValidator<LoginDto>
+    {
+        public LoginDtoValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required")
+                .EmailAddress().WithMessage("Invalid email format");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password is required")
+                .MinimumLength(6).WithMessage("Password must be at least 6 characters");
+        }
+    }
+}
